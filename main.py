700cff96e8626acfcf8690fe5f36642a818a39d3
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-02-26 19:24:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ semester = ['2013B', '2013J', '2014B', '2014J']
 with st.form("StudRegAndCourseForm", clear_on_submit=True):
     studentID = st.text_input("Enter student ID")
     code_presentation = st.selectbox('Select semester', semester)
-    selected_year = st.selectbox('Select year', year_list)
+    year = st.selectbox('Select year', year_list)
     withdrawnstatus = st.radio('Select a withdrawn status', ['0', '1'])
-    button = st.form_submit_button("Submit")
+    button = st.form_submit_button("<br>Submit")
+    
+st.write(studentID)
+st.write(withdrawnstatus)
+st.write(year)
+st.write(code_presentation)
