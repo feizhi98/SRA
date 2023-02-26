6973042d98d52c2e17c36bf663a4c7d011379f23
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-02-26 19:18:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-
 import streamlit as st
 import datetime
 
-st.title("Student Registation Form") 
+st.title("Student Registration Form")
 st.subheader("Enter details below")
 
-#Generate list of year
+# Generate list of years
 this_year = datetime.datetime.now().year
-year_list = list(range(this_year-10, this_year+1))
+year_list = list(range(this_year - 10, this_year + 1))
 
-with st.form("StudRegAndCourseForm", clear_on_submit=True): 
-	studentID = st.text_input("Enter student ID") 
-	code_presentation= st.text_input("Enter code_presentation(semester)") 
-withdrawnstatus = st.radio("Select an withdrawn status", ['0', '1'])
- 
-button= st.form_submit_button("Submit")
-
+with st.form("StudRegAndCourseForm", clear_on_submit=True):
+    studentID = st.text_input("Enter student ID")
+    code_presentation = st.text_input("Enter code_presentation (semester)")
+    selected_year = st.selectbox('Select year', year_list)
+    withdrawnstatus = st.radio('Select a withdrawn status', ['0', '1'])
+    button = st.form_submit_button("Submit")
