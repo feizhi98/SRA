56e28ad96c792e54ee66f703b0f2f56668a6615b
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-02-26 19:04:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,4 @@ with st.form("form1", clear_on_submit=True):
 	name = st.text_input("Enter full name") 
 	email = st.text_input("Enter email") 
 	message = st.text_area("Message") 
-	st.write(age)
 	button= st.form_submit_button("Submit")
