8c469d201cf4d42e077d60a84fbf13fb36de12c0
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-02-27 13:55:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -82,28 +82,28 @@ def student_info():
     st.title("Student Info Form")
     st.subheader("Enter details below")
 
-    year_list = ['2013','2014']
+    year_list = ['2013', '2014']
     semester = ['2013B', '2013J', '2014B', '2014J']
-    ageband = ['0-35','35-55','55<=']
-    numofattempts =['1','2','3','4','5','6']
-    education = ['HE Qualification','Lower Than A Level','A Level or Equivalent','Post Graduate Qualification','No Formal quals']
-    code_module = ['AAA','BBB','CCC','DDD','EEE','FFF','GGG']
-    region_ = ['East Aglian Region','East Midlands Region','Ireland','London Region','North Region','North Western Region','Scotland','South East Region','South Region','South West Region','Wales','West Midlands Region','Yorkshire Region']
+    ageband = ['0-35', '35-55', '55<=']
+    numofattempts = ['1', '2', '3', '4', '5', '6']
+    education = ['HE Qualification', 'Lower Than A Level', 'A Level or Equivalent', 'Post Graduate Qualification', 'No Formal quals']
+    code_module = ['AAA', 'BBB', 'CCC', 'DDD', 'EEE', 'FFF', 'GGG']
+    region_ = ['East Anglian Region', 'East Midlands Region', 'Ireland', 'London Region', 'North Region', 'North Western Region', 'Scotland', 'South East Region', 'South Region', 'South West Region', 'Wales', 'West Midlands Region', 'Yorkshire Region']
 
     with st.form("StudentInfoForm", clear_on_submit=True):
-	studentID = st.text_input("Enter student ID")
-	 gender = st.radio('Select gender, ['M', 'F'])
-	 age_band = st.selectbox('Select age band', ageband)
-	 highest_education = st.selectbox('Select highest education level', education)
-	 region = st.selectbox('Select region', region_)
-	 disability = st.radio('Select disability status, ['Y', 'N'])
-	 num_of_prev_attempts = st.selectbox('Select number of previous attempts', numofattempts)
-	code_presentation = st.selectbox('Select semester', semester)
-		course = st.selectbox('Select code module', code_module)	
-		sum_click = st.text_input("Enter sum of clicks")
-		After_Clicks = st.text_input("Enter sum of clicks")
+        studentID = st.text_input("Enter student ID")
+        gender = st.radio('Select gender', ['M', 'F'])
+        age_band = st.selectbox('Select age band', ageband)
+        highest_education = st.selectbox('Select highest education level', education)
+        region = st.selectbox('Select region', region_)
+        disability = st.radio('Select disability status', ['Y', 'N'])
+        num_of_prev_attempts = st.selectbox('Select number of previous attempts', numofattempts)
+        code_presentation = st.selectbox('Select semester', semester)
+        course = st.selectbox('Select code module', code_module)
+        sum_click = st.text_input("Enter sum of clicks")
+        After_Clicks = st.text_input("Enter sum of clicks")
         Before_Clicks = st.text_input("Enter sum of clicks")
-        final_result = st.radio('Select result', ['Distinction','Pass', 'Fail','Withdrawn'])
+        final_result = st.radio('Select result', ['Distinction', 'Pass', 'Fail', 'Withdrawn'])
         studied_credits = st.text_input("Enter studied credits")
      
         button = st.form_submit_button("Submit")
@@ -114,24 +114,25 @@ def student_info():
                 st.write(gender)
             if age_band:
                 st.write(age_band)
-			if highest_education:
+            if highest_education:
                 st.write(highest_education)
             if region:
                 st.write(region)
             if disability:
                 st.write(disability)
-			if num_of_prev_attempts:
+            if num_of_prev_attempts:
                 st.write(num_of_prev_attempts)
-			if code_presentation:
+            if code_presentation:
                 st.write(code_presentation)
-			if course:
+            if course:
                 st.write(course)
-			st.write(sum_click)
-			st.write(After_Clicks)
-			st.write(Before_Clicks)
+            st.write(sum_click)
+            st.write(After_Clicks)
+            st.write(Before_Clicks)
             if final_result:
                 st.write(final_result)
-			st.write(studied_credits)
+            st.write(studied_credits)
+
 
 # Create the menu items and their respective pages
 menu_items = {
